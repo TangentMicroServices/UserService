24b152cf8b980843fe9eb92418ee1b2a479ca339
Christo Crampton <info@38.co.za>
2014-10-08 14:08:45 +0200

    ignoring some irrelevant pylints

--- account/monkey_patching.py
+++ account/monkey_patching.py
@@ -1,3 +1,6 @@
+# pylint: disable=E1101
+# pylint: disable=C0111
+
 from django.contrib.auth.models import User
 from account.models import Role
 from rest_framework.authtoken.models import Token
@@ -6,7 +9,7 @@ from rest_framework.authtoken.models import Token
 def add_role(self, role_name):
 
     if self.is_authenticated():
-        role, created = Role.objects.get_or_create(role_name=role_name)
+        role, created = Role.objects.get_or_create(role_name=role_name) # pylint: disable=W0612
         self.roles.add(role)
 
 
@@ -19,13 +22,12 @@ def has_role(self, role_name):
     return False
 
 
-def get_token(self):
+def get_token(self): 
 
     if self.is_authenticated():
         return Token.objects.get(user=self).key
     return None
 
 
-User.add_to_class("add_role", add_role)
 User.add_to_class("has_role", has_role)
 User.add_to_class("get_token", get_token)
