94f06cff8b34a1c7a4711904a782719af0905b41
Ian Roberts <ian@tangentsolutions.co.za>
2015-03-18 22:05:59 +0200

    Updated urls to include versioning in API

--- userservice/urls.py
+++ userservice/urls.py
@@ -3,14 +3,8 @@ from api.api import router
 from django.contrib import admin
 
 urlpatterns = patterns('',
-	url(r'^', include(router.urls)),
-	url(r'^api-token-auth/',
-	   'rest_framework.authtoken.views.obtain_auth_token'),
-
+	url(r'^api/v1/', include(router.urls)),
+	url(r'^api-token-auth/','rest_framework.authtoken.views.obtain_auth_token'),
 	url(r'^api-explorer/', include('rest_framework_swagger.urls')),
-
-	# url(r'^$', 'userService.views.home', name='home'),
-	# url(r'^blog/', include('blog.urls')),
-
 	url(r'^admin/', include(admin.site.urls)),
-	)
+)
