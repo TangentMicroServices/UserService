8ca4f130ca41b5e95f712aeaa907a56bf34d4be2
toast38coza <info@38.co.za>
2016-02-18 10:32:19 +0200

    move sites above auth

--- userservice/settings.py
+++ userservice/settings.py
@@ -20,13 +20,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    
 
     # 3rd party
     'rest_framework',
